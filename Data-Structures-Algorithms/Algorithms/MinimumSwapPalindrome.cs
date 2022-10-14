94becc1d95c273783a6661297f3b5d5e7cd9daa4
Felipe Salvador <f.salvador91@gmail.com>
2022-10-13 21:40:47 -0300

    Update MinimumSwapPalindrome.cs

--- Data-Structures-Algorithms/Algorithms/MinimumSwapPalindrome.cs
+++ Data-Structures-Algorithms/Algorithms/MinimumSwapPalindrome.cs
@@ -6,50 +6,100 @@ namespace Data_Structures_Algorithms.Algorithms
     {
         static void Main(string[] args)
         {
-            var input = "001010";
-            Console.WriteLine(MinimumSwap(input));
+            var string = "001010";
+            Console.WriteLine(MinMovesToMakePalindrome(string));
         }
 
-        static int MinimumSwap(string s)
+        static int MinMovesToMakePalindrome(String s)
         {
-            var count = 0;
-            var l = 0;
-            var r = s.Length - 1;
-            var sb = new StringBuilder(s);
-            var skip = 0;
-            while (l < r)
+            var ans = 0;
+            var firstPointer = 0;
+            var end = s.Length - 1;
+            char[] ch = s.ToCharArray();
+
+
+            if (CanFormPalindrome(s) == false)
+                return -1;
+
+            while (firstPointer < end)
             {
-                var leftChar = sb[l];
-                var temp = r;
-                while (l < temp && leftChar != sb[temp])
+                var secondPointer = end;
+                if (ch[firstPointer] == ch[end])
                 {
-                    temp--;
-                }
-                if (l == temp)
-                {
-                    if (s.Length % 2 == 0)
-                    {
-                        return -1;
-                    }
-                    else
-                    {
-                        if (skip == 1)
-                        {
-                            return -1;
-                        }
-                        skip = 1;
-                        count = count + (s.Length / 2) - l;
-                        l++;
-                    }
+                    Console.WriteLine($"First pointer ({ch[firstPointer]}) is equal to the last ({ch[end]}) ");
+                    firstPointer++;
+                    end--;
+
+                    if (firstPointer < end)
+                        Console.WriteLine($"Nothing happens. First pointer is now index {firstPointer} and second pointer is index {end}");
+
                     continue;
                 }
-                count = count + r - temp;
-                sb.Remove(temp,1);
-                sb.Insert(r, leftChar);
-                r--;
-                l++;
+
+                while (ch[firstPointer] != ch[secondPointer])
+                {
+                    Console.WriteLine($"First pointer ({ch[firstPointer]}) in the index {firstPointer} is different to second pointer({ch[secondPointer]}) in the index {secondPointer}, so the second pointer will jump to index {secondPointer - 1}");
+                    secondPointer--;
+                }
+
+
+                if (firstPointer == secondPointer)
+                {
+                    Swap(ch, secondPointer, secondPointer + 1); // Change one char with the secondPointermost one
+                    ans++;
+                }
+                else
+                {
+                    while (secondPointer < end)
+                    {
+                        Swap(ch, secondPointer, secondPointer + 1); // Change one char with the secondPointermost one
+                        ans++;
+                        secondPointer++;
+                    }
+                }
             }
-            return count; 
+
+            return ans;
+        }
+
+        static void Swap(char[] ch, int i, int j)
+        {
+            Console.WriteLine("The char" + ch[j] + " of index " + j + " are goint to index " + i + " that was the char: " + ch[i] + $".The index {i} will receive the char {ch[j]}");
+            var temp = ch[i];
+            ch[i] = ch[j];
+            ch[j] = temp;
+        }
+
+        static bool CanFormPalindrome(string str)
+        {
+            int NO_OF_CHARS = 256;
+
+            // Create a count array and initialize all
+            // values as 0
+            int[] count = new int[NO_OF_CHARS];
+            Array.Fill(count, 0);
+
+            // For each character in input strings,
+            // increment count in the corresponding
+            // count array
+            for (int i = 0; i < str.Length; i++)
+            {
+                count[(int)(str[i])]++;
+            }
+
+            // Count odd occurring characters
+            int odd = 0;
+            for (int i = 0; i < NO_OF_CHARS; i++)
+            {
+                if ((count[i] & 1) == 1)
+                    odd++;
+
+                if (odd > 1)
+                    return false;
+            }
+
+            // Return true if odd count is 0 or 1,
+            return true;
         }
     }
 }
