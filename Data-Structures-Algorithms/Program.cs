2c6f9231739903e3a3d2ac988be3a1a1285cf867
Felipe Salvador <f.salvador91@gmail.com>
2022-11-11 19:44:31 -0300

    loop linked list

--- Data-Structures-Algorithms/Program.cs
+++ Data-Structures-Algorithms/Program.cs
@@ -7,7 +7,7 @@ namespace data_structure_algorithms
     {
         static void Main(string[] args)
         {
-            new LongestCommonSubSequence().Test();
+            new LoopLinkeList().Test();
         }
     }
 }
