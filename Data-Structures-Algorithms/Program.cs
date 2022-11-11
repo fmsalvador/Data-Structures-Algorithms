5adc9bc61ccfc4609f5ce6d58262b8105be8981e
Felipe Salvador <f.salvador91@gmail.com>
2022-11-11 19:33:40 -0300

    longest common substring/subsequence

--- Data-Structures-Algorithms/Program.cs
+++ Data-Structures-Algorithms/Program.cs
@@ -7,7 +7,7 @@ namespace data_structure_algorithms
     {
         static void Main(string[] args)
         {
-            new FruitIntoBasket().Test();
+            new LongestCommonSubSequence().Test();
         }
     }
 }
