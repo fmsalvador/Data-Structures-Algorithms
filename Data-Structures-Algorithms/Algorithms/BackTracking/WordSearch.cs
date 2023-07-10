929a4e7b2d354ff706268885fb875f40430843ab
Felipe Matheus Salvador <null>
2023-07-10 20:29:34 -0300

    refactoring

--- Data-Structures-Algorithms/Algorithms/BackTracking/WordSearch.cs
+++ Data-Structures-Algorithms/Algorithms/BackTracking/WordSearch.cs
@@ -3,9 +3,10 @@
 namespace Data_Structures_Algorithms.Algorithms
 {
     //Backtracking Recursion with Breadth First Search
-    internal class WordSearch
+    public class WordSearch
     {
         //O(MxN)
+        //TC:(MxN)
         public void Test()
         {
             char[] sequence1 = { 'A', 'B', 'C', 'E' };
@@ -13,9 +14,9 @@ namespace Data_Structures_Algorithms.Algorithms
             char[] sequence3 = { 'A', 'D', 'E', 'E' };
             char[][] board = { sequence1, sequence2, sequence3 };
 
-            var result1 = exist(board, "ABCB"); //False
-            var result2 = exist(board, "SEE"); //True
-            var result3 = exist(board, "ABCCED"); //True
+            var result1 = Exist(board, "ABCB"); //False
+            var result2 = Exist(board, "SEE"); //True
+            var result3 = Exist(board, "ABCCED"); //True
 
             Console.WriteLine(result1);
             Console.WriteLine(result2);
@@ -24,7 +25,7 @@ namespace Data_Structures_Algorithms.Algorithms
         }
 
         bool[][] visited;
-        public bool Exist(char[][] board, string word)
+        private bool Exist(char[][] board, string word)
         {
             visited = new bool[board.Length][];
             for (int i = 0; i < board.Length; i++)
@@ -67,37 +68,5 @@ namespace Data_Structures_Algorithms.Algorithms
             visited[i][j] = false;
             return false;
         }
-
-        public bool exist(char[][] board, String word)
-        {
-            for (int i = 0; i < board.Length; i++)
-            {
-                for (int j = 0; j < board[0].Length; j++)
-                {
-                    if (board[i][j] == word[0] && this.DFS(board, i, j, 0, word))
-                    {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-        public bool DFS(char[][] board, int i, int j, int count, String word)
-        {
-            if (count == word.Length)
-            {
-                return true;
-            }
-            if (i < 0 || j < 0 || i >= board.Length || j >= board[0].Length || board[i][j] != word[count])
-            {
-                return false;
-            }
-            var temp = board[i][j];
-            board[i][j] = ' ';
-            var found = this.DFS(board, i + 1, j, count + 1, word) || this.DFS(board, i - 1, j, count + 1, word) || this.DFS(board, i, j + 1, count + 1, word) || this.DFS(board, i, j - 1, count + 1, word);
-            board[i][j] = temp;
-            return found;
-        }
-        // TC: O(m * n)
     }
 }
